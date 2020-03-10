076d1bf84afbe16dbe05977c05ea8d4025011d54
Gon Gis <gon.eu.1987@gmail.com>
2020-03-10 14:38:27 +0100

    finishing the code for initializing the data

--- src/components/AppComponent.js
+++ src/components/AppComponent.js
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { handleInitialData } from '../actions/shared';
 import { connect } from 'react-redux';
 import NavComponent from './NavComponent';
 
 class AppComponent extends Component {
 
-
+    componentDidMount() {
+        this.props.dispatch(handleInitialData());
+    }
     render() {
         return(
             <Router>
@@ -22,8 +25,10 @@ class AppComponent extends Component {
     }
 }
 
-function mapStateToProps(){
-    return {}
+function mapStateToProps({authedUser}){
+    return {
+        loading: authedUser === null
+    }
 }
 
 export default connect(mapStateToProps)(AppComponent)
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './components/AppComponent';
-import { createStore } from "redux";
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import middleware from './middleware';
 
-const store = createStore(reducer, middleware);
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(middleware));
+  
 ReactDOM.render(
     <Provider store={store}>
         <App />
